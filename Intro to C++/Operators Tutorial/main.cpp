80d54d8dbcac4046cf8eeb1149d2c2be6536ebe9
Ryan Ashtari <ryan.ashtari@gmail.com>
2025-02-03 22:41:20 +1100

    Completed the Operators Tutorial

---
+++ Intro to C++/Operators Tutorial/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+void main()
+{
+    int first = 0;
+    int second = 0;
+
+    std::cout << "Enter the first number: " << std::endl;
+    std::cin >> first;
+
+    if (std::cin.fail()) {
+        std::cout << "You didn't enter an integer. We'll just use '5' instead." << std::endl;
+        first = 5;
+        std::cin.clear();
+        std::cin.ignore(std::cin.rdbuf()->in_avail());
+    }
+
+    std::cout << "Enter the second number: " << std::endl;
+    std::cin >> second;
+
+    if (std::cin.fail()) {
+        std::cout << "You didn't enter an integer. We'll just use '8' instead."
+            << std::endl;
+        second = 8;
+        std::cin.clear();
+        std::cin.ignore(std::cin.rdbuf()->in_avail());
+    }
+    std::cout << "The two numbers entered are: " << first << " and " << second << "." << std::endl;
+    std::cout << first << " + " << second << " = " << (first + second) << std::endl;
+    std::cout << first << " - " << second << " = " << (first - second) << std::endl;
+    std::cout << first << " / " << second << " = " << (first / second) << std::endl;
+    std::cout << first << " * " << second << " = " << (first * second) << std::endl;
+    std::cout << first << " % " << second << " = " << (first % second) << std::endl;
+
+    std::cin.ignore(std::cin.rdbuf()->in_avail());
+    std::cin.get();
+}
