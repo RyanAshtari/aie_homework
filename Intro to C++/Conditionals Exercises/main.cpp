7e8a7b125bd997665e138ca396c5a659a0bbed97
Ryan Ashtari <ryan.ashtari@gmail.com>
2025-02-04 23:01:44 +1100

    Completed the Conditionals Exercise

--- Intro to C++/Conditionals Exercises/main.cpp
+++ Intro to C++/Conditionals Exercises/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
@@ -39,15 +40,15 @@ int main()
 
 	if (numTotal == 0)
 	{
-		std::cout << "Zero";
+		std::cout << "Zero\n";
 	}
 	else if (numTotal % 2 == 0)
 	{
-		std::cout << "Positive";
+		std::cout << "Positive\n";
 	}
 	else
 	{
-		std::cout << "Negative";
+		std::cout << "Negative\n";
 	}
 
 	int choice = 4;
@@ -55,21 +56,101 @@ int main()
 	switch (choice)
 	{
 	case 1:
-		std::cout << "1";
+		std::cout << "1\n";
 		break;
 	case 2:
-		std::cout << "2 or 3";
+		std::cout << "2 or 3\n";
 		break;
 	case 3:
-		std::cout << "2 or 3";
+		std::cout << "2 or 3\n";
 		break;
 	case 4:
-		std::cout << "4";
+		std::cout << "4\n";
 		break;
 	default:
-		std::cout << "Invalid";
+		std::cout << "Invalid\n";
 	}
 
 	int result = (x == 0) ? y = 0 : y = 10 / x;
 
+	std::cout << "Please input one number: ";
+	int userInput1;
+	std::cin >> userInput1;
+	std::cout << "\nNow input a second number: ";
+	int userInput2;
+	std::cin >> userInput2;
+	std::cout << "\nFinally input one of these operators (+,-,*,/,%): ";
+	std::string userInputOperator;
+	std::cin >> userInputOperator;
+
+	if (userInputOperator == "+")
+	{
+		std::cout << userInput1 << " + " << userInput2 << " = " << userInput1 + userInput2;
+	}
+	else if (userInputOperator == "-")
+	{
+		std::cout << userInput1 << " - " << userInput2 << " = " << userInput1 - userInput2;
+	}
+	else if (userInputOperator == "*")
+	{
+		std::cout << userInput1 << " * " << userInput2 << " = " << userInput1 * userInput2;
+	}
+	else if (userInputOperator == "/")
+	{
+		std::cout << userInput1 << " / " << userInput2 << " = " << userInput1 / userInput2;
+	}
+	else if (userInputOperator == "%")
+	{
+		std::cout << userInput1 << " % " << userInput2 << " = " << userInput1 % userInput2;
+	}
+	else
+	{
+		std::cout << "\nError: one of your input's was not appropriate, the program will not run.";
+	}
+
+	std::cout << "\nPlease input a number from 1-12 representing a month of the year: ";
+	int userMonth;
+	std::cin >> userMonth;
+
+	switch (userMonth)
+	{
+	case 1:
+		std::cout << "\nThere are 31 days in January.";
+		break;
+	case 2:
+		std::cout << "\nThere are 28 days in February during a common year, and 29 days in a leap year.";
+		break;
+	case 3:
+		std::cout << "\nThere are 31 days in March.";
+		break;
+	case 4:
+		std::cout << "\nThere are 30 days in April.";
+		break;
+	case 5:
+		std::cout << "\nThere are 31 days in May.";
+		break;
+	case 6:
+		std::cout << "\nThere are 30 days in June.";
+		break;
+	case 7:
+		std::cout << "\nThere are 31 days in July.";
+		break;
+	case 8:
+		std::cout << "\nThere are 31 days in August.";
+		break;
+	case 9:
+		std::cout << "\nThere are 30 days in September.";
+		break;
+	case 10:
+		std::cout << "\nThere are 31 days in October.";
+		break;
+	case 11:
+		std::cout << "\nThere are 30 days in November.";
+		break;
+	case 12:
+		std::cout << "\nThere are 31 days in December.";
+		break;
+	default:
+		std::cout << "\nError! You have not selected a valid input. The program will not run.";
+	}
 }
