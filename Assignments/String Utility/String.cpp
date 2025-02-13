976dc61f86d43e8d756a3a68c4170e7d26ac19b5
Ryan Ashtari <s241882@students.aie.edu.au>
2025-02-13 17:00:23 +1100

    Started the String Util Assessment

---
+++ Assignments/String Utility/String.cpp
@@ -0,0 +1,57 @@
+#include "String.h"
+
+String::String()
+{
+}
+
+String::String(const char* _str)
+{
+}
+
+String::String(const String& _other)
+{
+}
+
+String::~String()
+{
+}
+
+size_t String::Length() const
+{
+	return size_t();
+}
+
+String& String::Append(const String& _str)
+{
+	// TODO: insert return statement here
+}
+
+String& String::ToLower()
+{
+	// TODO: insert return statement here
+}
+
+String& String::ToUpper()
+{
+	// TODO: insert return statement here
+}
+
+int String::FindCharacter(const char _chr)
+{
+	return 0;
+}
+
+int String::Replace(const char _find, const char _replace)
+{
+	return 0;
+}
+
+String& String::ReadFromConsole()
+{
+	// TODO: insert return statement here
+}
+
+String& String::WriteToConsole()
+{
+	// TODO: insert return statement here
+}
