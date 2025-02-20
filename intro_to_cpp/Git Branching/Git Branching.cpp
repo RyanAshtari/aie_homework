5b8f82c84891923927e24bfcb44209527809d7d9
Ryan Ashtari <s241882@students.aie.edu.au>
2025-02-20 11:33:54 +1100

    Adding missed git branching folder

---
+++ intro_to_cpp/Git Branching/Git Branching.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+    cout << "Welcome to the Australian Science program-aided enrichment center.\n";
+    cout << "We hope your time in the classroom has been a pleasant one.\n";
+    cout << "For your own safety and the safety of others, please ______.\n";
+
+    return 0;
+}
+
